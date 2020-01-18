90fcb3b9ca2d7bf33b8b87c62e607802bbebb0c0
Gefrey <shot.kolt@mail.ru>
2020-01-18 16:17:48 +0700

    create programm

---
+++ cmake-build-debug/CMakeFiles/3DShooter.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3DShooter.exe"
+  "3DShooter.exe.manifest"
+  "3DShooter.pdb"
+  "CMakeFiles/3DShooter.dir/main.cpp.obj"
+  "lib3DShooter.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3DShooter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
